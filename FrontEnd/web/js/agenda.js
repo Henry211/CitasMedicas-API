5d13020e102e606fc03963c0fd60c055c73170e7
Henry <henrygova@hotmail.com>
2022-06-04 17:42:24 -0600

    Vista de Agenda (en desarrollo)

---
+++ FrontEnd/web/js/agenda.js
@@ -0,0 +1,19 @@
+
+
+/* 0:lunes  1:martes  2:miercoles  3:jueves  4:viernes*/
+var horario = [lunes,martes,miercoles,jueves,viernes];
+
+var lunes = {checked:false};
+var martes = {checked:false};
+var miercoles = {checked:false};
+var jueves = {checked:false};
+var viernes = {checked:false};
+
+
+
+
+function main(){
+
+}
+
+$(main);
