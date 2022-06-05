39c5bcbe516e97d1dceb6108e3bf6868aa2b8ba4
MariaCruzC <maria.cruz@est.una.ac.cr>
2022-06-05 13:43:25 -0600

    Logic usuarios

---
+++ CitasMedicas-API/BackEnd/src/java/una/ac/backend/logic/Paciente.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package una.ac.backend.logic;
+
+/**
+ *
+ * @author ESCINF
+ */
+public class Paciente {
+    
+}
