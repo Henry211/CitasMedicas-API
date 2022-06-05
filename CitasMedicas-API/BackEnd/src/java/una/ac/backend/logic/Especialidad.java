badb3dafac9db45518decbed921d958cd6e07d4e
MariaCruzC <maria.cruz@est.una.ac.cr>
2022-06-05 15:00:14 -0600

    DoctorDao

--- CitasMedicas-API/BackEnd/src/java/una/ac/backend/logic/Especialidad.java
+++ CitasMedicas-API/BackEnd/src/java/una/ac/backend/logic/Especialidad.java
@@ -4,10 +4,43 @@
  */
 package una.ac.backend.logic;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 /**
  *
  * @author ESCINF
  */
-public class Especialidad {
+public class Especialidad implements Serializable{
+
+    String especialidad;
+
+    public Especialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public Especialidad() {
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
     
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + Objects.hashCode(this.especialidad);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Especialidad{" + "especialidad=" + especialidad + '}';
+    }
+    
+
 }
