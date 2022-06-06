6470b7699442bca733fc51fd1d8068afb3cf8f6b
Henry <henrygova@hotmail.com>
2022-06-06 10:48:05 -0600

    Backend

---
+++ projects/BackEnd/src/java/una/ac/backend/resources/RestConfiguration.java
@@ -0,0 +1,14 @@
+
+package una.ac.backend.resources;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author Henry
+ */
+@ApplicationPath("api")
+public class RestConfiguration extends Application{
+    
+}
