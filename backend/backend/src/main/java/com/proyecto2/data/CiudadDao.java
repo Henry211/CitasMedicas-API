5e1d11bab68b3331ec122401e9615df0b729593b
MariaCruzC <maria.cruz@est.una.ac.cr>
2022-06-09 17:48:10 -0600

    Localidad y especialidad Dao

---
+++ backend/backend/src/main/java/com/proyecto2/data/CiudadDao.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.proyecto2.data;
+
+import com.proyecto2.logic.Ciudad;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ESCINF
+ */
+public class CiudadDao {
+    
+    Database db;
+
+    public CiudadDao() {
+        db = Database.instance();
+    }
+
+    public void create(Ciudad u) throws Exception {
+        String sql = "insert into localidad (provincia) "
+                + "values(?)";
+        PreparedStatement stm = db.prepareStatement(sql);
+        stm.setString(1, u.getProvincia());
+        int count = db.executeUpdate(stm);
+        if (count == 0) {
+            throw new Exception("Localidad ya existe");
+        }
+    }
+
+    public Ciudad read(Ciudad u) throws Exception {
+        String sql = "select * from localidad c where provincia=?";
+        PreparedStatement stm = db.prepareStatement(sql);
+        stm.setString(1, u.getProvincia());
+        ResultSet rs = db.executeQuery(stm);
+        if (rs.next()) {
+            Ciudad c = from(rs, "c");
+            return c;
+        } else {
+            throw new Exception("Ciudad existente");
+        }
+
+    }
+    
+     public ArrayList<Ciudad> findAll() {
+        ArrayList<Ciudad> resultado = new ArrayList<>();
+        try {
+            String sql = "select * from localidad c";
+            PreparedStatement stm = db.prepareStatement(sql);
+            ResultSet rs = db.executeQuery(stm);
+            Ciudad c;
+            while (rs.next()) {
+                c = from(rs, "c");
+                resultado.add(c);
+            }
+        } catch (SQLException ex) {
+        }
+        return resultado;
+    }
+
+     
+      Ciudad from(ResultSet rs, String alias) {
+        try {
+            Ciudad c = new Ciudad();
+            c.setProvincia(rs.getString(alias + ".provincia"));
+            return c;
+        } catch (SQLException ex) {
+            return null;
+        }
+    }
+    
+}
---
+++ backend/backend/src/main/java/com/proyecto2/data/EspecialidadDao.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.proyecto2.data;
+
+import com.proyecto2.logic.Especialidad;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ESCINF
+ */
+public class EspecialidadDao {
+    Database db;
+
+    public EspecialidadDao() {
+        db = Database.instance();
+    }
+
+    public void create(Especialidad u) throws Exception {
+        String sql = "insert into especialidad (especialidad) " 
+                + "values(?)";
+        PreparedStatement stm = db.prepareStatement(sql);
+        stm.setString(1, u.getEspecialidad());
+        int count = db.executeUpdate(stm);
+        if (count == 0) {
+            throw new Exception("Especialidad ya existe");
+        }
+    }
+
+    public Especialidad read(Especialidad u) throws Exception {
+        String sql = "select * from especialidad c where especialidad=? ";
+        PreparedStatement stm = db.prepareStatement(sql);
+        stm.setString(1, u.getEspecialidad());
+        ResultSet rs = db.executeQuery(stm);
+        if (rs.next()) {
+            Especialidad c = from(rs, "c");
+            return c;
+        } else {
+            throw new Exception("Especialidad existente");
+        }
+    }
+
+    public void update(Especialidad u) throws Exception {
+        String sql = "update especialidad set especialidad=?" + "where especialidad=?";
+        PreparedStatement stm = db.prepareStatement(sql);
+        stm.setString(1, u.getEspecialidad());
+        int count = db.executeUpdate(stm);
+        if (count == 0) {
+            throw new Exception("Especialidad no existe");
+        }
+    }
+    
+        public ArrayList<Especialidad> findAll() {
+        ArrayList<Especialidad> resultado = new ArrayList<>();
+        try {
+            String sql = "select * from especialidad c";
+            PreparedStatement stm = db.prepareStatement(sql);
+            ResultSet rs = db.executeQuery(stm);
+            Especialidad c;
+            while (rs.next()) {
+                c = from(rs, "c");
+                resultado.add(c);
+            }
+        } catch (SQLException ex) {
+        }
+        return resultado;
+    }
+
+    Especialidad from(ResultSet rs, String alias) {
+        try {
+            Especialidad c = new Especialidad();
+            c.setEspecialidad(rs.getString(alias + ".especialidad"));
+            return c;
+        } catch (SQLException ex) {
+            return null;
+        }
+    }
+    
+}
