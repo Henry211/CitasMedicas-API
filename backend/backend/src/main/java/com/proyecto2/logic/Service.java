4e9d0c20c885fd823c02ed3698ee487c3db69bcc
MariaCruzC <maria.cruz@est.una.ac.cr>
2022-06-10 14:38:33 -0600

    service2

--- backend/backend/src/main/java/com/proyecto2/logic/Service.java
+++ backend/backend/src/main/java/com/proyecto2/logic/Service.java
@@ -4,10 +4,67 @@
  */
 package com.proyecto2.logic;
 
-/**
- *
- * @author ESCINF
- */
+import com.proyecto2.data.AdministradorDao;
+import com.proyecto2.data.CitaDao;
+import com.proyecto2.data.CiudadDao;
+import com.proyecto2.data.DoctorDao;
+import com.proyecto2.data.EspecialidadDao;
+import com.proyecto2.data.PacienteDao;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Service {
-    
+
+    PacienteDao pDao = new PacienteDao();
+    DoctorDao mDao = new DoctorDao();
+    AdministradorDao aDao = new AdministradorDao();
+    CiudadDao cidDao = new CiudadDao();
+    CitaDao citDao = new CitaDao();
+    EspecialidadDao esDao = new EspecialidadDao();
+    //HorarioDao horDao = new HorarioDao();
+
+    public Service() {
+    }
+
+    // Singleton implementation
+    private static Service theInstance;
+
+    public static Service instance() {
+        if (theInstance == null) {
+            theInstance = new Service();
+        }
+        return theInstance;
+    }
+
+    // Service methods
+    //paciente
+   
+
+    public void createPaciente(Paciente paciente) throws Exception {
+        pDao.create(paciente);
+    }
+
+    //administrador
+    public Administrador administradorLogin(Administrador administrador) throws Exception {
+        return aDao.read(administrador);
+    }
+
+    public void createAdministrador(Administrador admi) throws Exception {
+        aDao.create(admi);
+    }
+
+    //medico
+    public Doctor medicoLogin(Doctor medico) throws Exception {
+        return mDao.read(medico);
+    }
+
+    public void editarMedico(Doctor medico) throws Exception {
+        mDao.update(medico);
+    }
+
+    public void createMedico(Doctor medico) throws Exception {
+        mDao.create(medico);
+    }
+
 }
+
