d2ed3dd05f9a30940cc74ae57a7881fe711fb966
MariaCruzC <maria.cruz@est.una.ac.cr>
2022-06-10 14:19:51 -0600

    Service

--- backend/backend/src/main/java/com/proyecto2/logic/Horario.java
+++ backend/backend/src/main/java/com/proyecto2/logic/Horario.java
@@ -9,5 +9,45 @@ package com.proyecto2.logic;
  * @author ESCINF
  */
 public class Horario {
+    boolean checked;
+    int desde;
+    int hasta;
+
+    public Horario(boolean checked, int desde, int hasta) {
+        this.checked = checked;
+        this.desde = desde;
+        this.hasta = hasta;
+    }
+    
+    
+
+    public boolean isChecked() {
+        return checked;
+    }
+
+    public void setChecked(boolean checked) {
+        this.checked = checked;
+    }
+
+    public int getDesde() {
+        return desde;
+    }
+
+    public void setDesde(int desde) {
+        this.desde = desde;
+    }
+
+    public int getHasta() {
+        return hasta;
+    }
+
+    public void setHasta(int hasta) {
+        this.hasta = hasta;
+    }
+    
+    
+    
+            
+            
     
 }
